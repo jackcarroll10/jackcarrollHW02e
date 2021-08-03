28ba4c5b0cdaa16d47f6948fdda42d99a9f21ecd
jackc <kenneth.carroll@seattlecolleges.edu>
2021-08-03 11:11:09 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('''Rectangle program
+You will be prompted for the length and width
+of a rectangle below.''')
+length = int(input('Enter the length: '))
+width = int(input('Enter the width:   '))
+print('=============')
+print('Area =      ', length * width)
+print('Perimeter = ', (2 * length) + (2 * width))
+print('==============\nThanks for using this program!')
